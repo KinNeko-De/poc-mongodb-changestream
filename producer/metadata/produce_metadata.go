b0cecf2faf35cddb265ecb050777508c7783a78d
Nils Diekmann <github@kinneko.de>
2025-06-08 14:09:00 +0200

    - endless producing

--- producer/metadata/produce_metadata.go
+++ producer/metadata/produce_metadata.go
@@ -8,12 +8,13 @@ import (
 
 func ProduceFileMetadata(ctx context.Context) error {
 	fmt.Println("Producing file metadata...")
-	select {
-	case <-ctx.Done():
-		fmt.Println("Context cancelled, producing metadata stopped")
-		return ctx.Err()
-	case <-time.After(20 * time.Second):
-		fmt.Println("File metadata produced")
-		return nil // 5 seconds passed, action completed
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Context cancelled, producing metadata stopped")
+			return ctx.Err()
+		case <-time.After(20 * time.Second):
+			fmt.Println("File metadata produced")
+		}
 	}
 }
