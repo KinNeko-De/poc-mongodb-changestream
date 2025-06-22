e04a14c0083adeeb07329a3687e2aca34d0423ad
Nils Diekmann <github@kinneko.de>
2025-06-22 02:51:13 +0200

    . generate protobuf message

---
+++ golang/store_file/go.mod
@@ -0,0 +1,5 @@
+module github.com/kinneko-de/poc-mongodb-changestream/golang/store_file
+
+go 1.24.3
+
+require google.golang.org/protobuf v1.36.6
---
+++ scripts/proto/generate-go.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+export PATH="$PATH:$(go env GOPATH)/bin"
+
+protobase=proto
+outputpath=golang
+
+cd ..
+cd ..
+
+mkdir -p ${outputpath}
+cd ${outputpath}
+outdated=( $(\ls -d */) )
+for o in "${outdated[@]}"
+do
+    find ${o} -name *.pb.go \
+    -exec rm {} +
+    echo "Removed generated go code for $o"
+    rm ${o}/go.mod
+    rm ${o}/go.sum
+    echo "Removed generated go mod files for $o"
+done
+cd - > /dev/null
+
+cd ${protobase}
+projects=( $(\ls -d */ ) )
+cd - > /dev/null
+
+for i in "${projects[@]}"
+do
+    find ${protobase}/${i} -name *.proto \
+    -exec tools/protoc/win64/bin/protoc.exe --proto_path=${protobase} --go_out=${outputpath} --go_opt=paths=source_relative {} +
+    echo "Generated go code for: $i"
+	
+	cd ${outputpath}/${i}
+	modulename=$(echo github.com/kinneko-de/poc-mongodb-changestream/golang/${i} | sed 's/.$//')
+	go mod init ${modulename}
+	go mod tidy
+	cd - > /dev/null
+	echo "Generated mod files for: $i"
+done
+
+read -p "Press [Enter] to exit."
